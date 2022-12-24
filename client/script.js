c83d73d830596b2fef7f3c484055eba2481c3c06
Leif-Francisco <105963035+Leif-Francisco@users.noreply.github.com>
2022-12-25 06:07:59 +0800

    Added base front-end

---
+++ client/script.js
@@ -0,0 +1,89 @@
+import bot from './assets/assets/bot.svg'
+import user from './assets/assets/user.svg'
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container')
+
+let loadInterval;
+
+function loader(element){
+  element.textContent= '';
+
+  loadInterval = setInterval(() =>{
+    element.textContent += '.';
+
+    if(element.textContent === '....'){
+        element.textContent = '';
+    }
+  }, 300)
+
+}
+
+function typeText(element,text) {
+    let index = 0;
+
+    let interval = setInterval(() => {
+        if(index < text.length) {
+            element.innerHTML += text.chartAt(index);
+            index++;
+        } else {
+            clearInterval(interval);
+        }
+
+    }, 20)
+}
+
+function generateUniqueId(){
+    const timestamp = Date.now();
+    const randomNumber = Math.random();
+    const hexadecimalString = randomNumber.toString(16);
+    
+    return `id-${timestamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAi, value, uniqueID) {
+    return (
+        `
+            <div class="wrapper" ${isAi && 'ai'}">
+                <div class="chat">
+                    <div classname="profile">
+                        <img
+                            src="${isAi ? bot : user}"
+                            alt="${isAi ? 'bot' : 'user'}"
+                        />
+                    </div>
+                    <div class="message" id=${uniqueID}>${value}></div>
+                </div>
+
+            </div>
+        `
+    )
+}
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    //user's chatstripe
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+    form.reset();
+
+    const uniqueID = generateUniqueId();
+    chatContainer.innerHTML += chatStripe(true," ", uniqueID);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+    const messageDiv = document.getElementById(uniqueID);
+
+    loader(messageDiv);
+}
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) =>{
+    if (e.keyCode ===13) {
+        handleSubmit(e);
+    }
+    
+})
